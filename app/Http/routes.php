61222f3b684d515159bab385a993297117f330e4
Konstantin <pilot911@yandex.ru>
2015-10-20 10:13:57 +0300

    Last updates

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,14 +1,4 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-//\Route::get('/', 'WelcomeController@index');
+    get('{locale?}', ['as' => 'index', 'uses' => '\App\Http\Controllers\Site\HomeController@index'])
+        ->where('locale', ($c = config('app.locales')) ? $c->implode('|') : '');
