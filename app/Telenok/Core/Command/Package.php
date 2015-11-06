83ef5eace1821334db756acb167d9ca13569d5df
Konstantin <pilot911@yandex.ru>
2015-11-07 00:31:49 +0300

    Last updates

---
+++ app/Telenok/Core/Command/Package.php
@@ -0,0 +1,5 @@
+<?php namespace App\Telenok\Core\Command;
+
+class Package extends \Telenok\Core\Command\Package {
+
+}
--- app/Telenok/Core/Command/PackagesUpdate.php
+++
@@ -1,5 +0,0 @@
-<?php namespace App\Telenok\Core\Command;
-
-class PackagesUpdate extends \Telenok\Core\Command\PackagesUpdate {
-
-}
