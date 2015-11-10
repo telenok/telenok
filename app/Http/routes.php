00ee36001d8f09fdadc9581f2242be306a28c25e
Konstantin <pilot911@yandex.ru>
2015-11-10 04:13:17 +0300

    Last updates

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,4 +1,5 @@
 <?php
-
+/*
     get('{locale?}', ['as' => 'index', 'uses' => '\App\Http\Controllers\Site\HomeController@index'])
         ->where('locale', ($c = config('app.locales')) ? $c->implode('|') : '');
+*/
--- config/image.php
+++ config/image.php
@@ -16,7 +16,7 @@ return [
 		'quality' => 85,
 		'jpeg_quality' => 85, // [0 .. 100]
 		'png_compression_level' => 5, // [0 ... 9]
-		'png_compression_filter' => PNG_ALL_FILTERS, // [PNG_FILTER_NONE, PNG_FILTER_SUB, PNG_FILTER_UP, PNG_FILTER_AVG, PNG_FILTER_PAETH, PNG_ALL_FILTERS]
+		// 'png_compression_filter' => PNG_ALL_FILTERS, // [PNG_FILTER_NONE, PNG_FILTER_SUB, PNG_FILTER_UP, PNG_FILTER_AVG, PNG_FILTER_PAETH, PNG_ALL_FILTERS]
 		// Other option please look at http://imagine.readthedocs.org/en/latest/index.html
 	]
 ];
