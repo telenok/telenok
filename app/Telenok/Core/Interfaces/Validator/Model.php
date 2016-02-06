183676b257837d17b3165be473035290508915f2
Konstantin <pilot911@yandex.ru>
2016-02-07 00:06:44 +0300

    Last updates

--- app/Telenok/Core/Interfaces/Validator/Model.php
+++
@@ -1,5 +0,0 @@
-<?php namespace App\Telenok\Core\Interfaces\Validator;
-
-class Model extends \Telenok\Core\Interfaces\Validator\Model {
-	
-}
---
+++ app/Telenok/Core/Support/File/Processing.php
@@ -0,0 +1,4 @@
+<?php namespace App\Telenok\Core\Support\File;
+
+class Processing extends \Telenok\Core\Support\File\Processing {
+}
