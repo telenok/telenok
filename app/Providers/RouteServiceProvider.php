4792c66604e875917f7a1a799f02d1724f4ade71
Konstantin <pilot911@yandex.ru>
2016-03-08 20:56:05 +0300

    Last updates

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,7 +39,7 @@ class RouteServiceProvider extends ServiceProvider {
     {
         $router->group(['namespace' => $this->namespace], function($router)
         {
-            require storage_path('telenok/route/route.php');
+            require app_path('Http/routes.php');
         });
     }
 }
