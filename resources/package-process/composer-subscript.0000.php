fc3583200a8f28d65bbb3bb3ca98187cc1b81382
Konstantin <pilot911@yandex.ru>
2016-06-25 22:42:47 +0300

    New commit

--- resources/package-process/composer-subscript.0000.php
+++ resources/package-process/composer-subscript.0000.php
@@ -1,3 +1,3 @@
 <?php
 
-    \Telenok\Core\Support\Install\ComposerScripts::recursiveCopy(__DIR__ . "/../../", __DIR__ . "/../../../../");
+    \Telenok\Core\Support\Install\ComposerScripts::recursiveCopy(__DIR__ . "/../../", __DIR__ . "/../../../../../");
