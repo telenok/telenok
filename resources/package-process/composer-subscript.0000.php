47520993b6c193e7ff5c29009fd6982ae99b8100
Konstantin <pilot911@yandex.ru>
2016-06-25 23:47:18 +0300

    New commit

--- resources/package-process/composer-subscript.0000.php
+++ resources/package-process/composer-subscript.0000.php
@@ -1,3 +1,7 @@
 <?php
 
     \Telenok\Core\Support\Install\ComposerScripts::recursiveCopy(__DIR__ . "/../../", __DIR__ . "/../../../../../");
+    \Telenok\Core\Support\Install\ComposerScripts::mergeRootComposerJson([
+        "classmap" => ["database"],
+        "psr-4" => ["App\\" => "app/"]
+    ]);
