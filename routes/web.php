6cf9a8798d5ee9f40ee38fc2e7e93729464aeb83
Konstantin <pilot911@yandex.ru>
2016-12-26 02:16:25 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,4 @@
 <?php
 
-app('router')->pattern('telenok_domain', '(' . implode('|',
-        \App\Vendor\Telenok\Core\Model\Web\Domain::active()->get()->transform(function($item)
-        {
-            return preg_quote($item->domain);
-        })->values()->all()
-    ). ')');
 
-app('router')->group(['domain' => '{telenok_domain}', 'middleware' => ['web']], function ()
-{
-    //app("router")->post("some-url", array("as" => "page.some-name", "uses" => '\App\Http\Controllers\Controller@someMethod'));
-
-    require 'telenok.php';
-});
+require 'telenok.php';
